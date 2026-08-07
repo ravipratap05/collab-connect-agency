d28a104870e4be5b53599f00f945b0d0b529e263
Ravi Pratap Singh <115611686+ravipratap05@users.noreply.github.com>
2026-08-07 13:58:48 +0530

    Update BASE_URL for sitemap to new domain

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://collab-connect-agency.lovable.app";
+const BASE_URL = "https://collabconnect.co.in";
 
 interface SitemapEntry {
   path: string;
